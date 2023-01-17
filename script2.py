1b7d37ac6c0a154542806de377bcf9f0b89afadb
Carter Tomlenovich <cartertomlenovich@gmail.com>
2023-01-17 11:13:16 -0500

    modify scripts
    Removed print from script1
    Added comment to script2

--- script2.py
+++ script2.py
@@ -1,2 +1,3 @@
+# Print these lines from zen of python
 print("Beautiful is better than ugly. Explicit is better than implicit.")
 print("Simple is better than complex. Complex is better than complicated.")
